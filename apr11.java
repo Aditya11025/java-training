ff546528144c32da99161bfee7e805e20514b471
ishant <ishantyadav243@gmail.com>
2025-04-11 10:32:53 +0530

    hello

---
+++ apr11.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class apr11 {
+    
+    public static void main(String arg[]){
+     int a,b;
+     System.out.println("enter the variable");
+     Scanner sc=new Scanner(System.in);
+     a=sc.nextInt();
+     b=sc.nextInt();
+     System.out.println("enter the operator");
+     char ope=sc.next().charAt(0);
+     sc.close();
+     switch (ope) {
+        case '+':
+    
+        System.out.println("Addition:"+(a+b));
+            break;
+        case '-':
+            System.out.println("Subtraction:"+(a-b));
+            break;
+        case '*':
+            System.out.println("Multiplication:"+(a*b));    
+            break;
+        case '/':
+            if(b==0){
+                System.out.println("invalid input");
+                break;
+            }  
+            else{
+                System.out.println("Division:"+(a/b));
+                break;
+            }  
+        default:
+               System.out.println("invalid operator");
+            break;
+     }
+    }
+}
